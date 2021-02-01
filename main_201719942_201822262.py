36d65a6b606c79d19a3ace12c11ee365fd73493c
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2021-01-31 21:01:58 -0500

    importacion

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -2,7 +2,17 @@
 #Manuel Gallegos Bustamante #Código: 201719942
 #Análisis y procesamiento de imágenes: Proyecto1 Entrega1
 #Se importan librerías que se utilizarán para el desarrollo del laboratorio
-#Intento 2!!!!
+from skimage.filters import threshold_otsu
+from scipy.io import loadmat
+import os
+import glob
+import numpy as np
+import skimage.io as io
+import requests
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+
+image_url="https://estaticos.muyinteresante.es/uploads/images/article/57a2ef2a5cafe82d7b8b4567/elefante_0.jpg"
 
 
 

b8b0792d3a82fb925310fed305e5842bcf1db60d
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2021-01-31 21:19:31 -0500

    esquema subplot

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -13,14 +13,34 @@ from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
 image_url="https://estaticos.muyinteresante.es/uploads/images/article/57a2ef2a5cafe82d7b8b4567/elefante_0.jpg"
+r=requests.get(image_url)
+with open("Elefantes", "wb") as f: # se trabaja con f como la abreviación para abrir un archivo para escritura "Elefantes"
+	f.write(r.content) #se escribe con .write en el archivo previamente mencionado el contenido de la descarga de la imagen realizado previamente con .content
+carga_imagen=io.imread("Elefantes") # se carga la imagen del archivo creado con io.imread
 
+plt.figure("VisualizaciónAnotaciones")
+plt.subplot(3,2,1)
+plt.title("Imagen a color")
+plt.imshow(carga_imagen)
+plt.axis("off")
+plt.tight_layout()
+plt.subplot(3,2,3)
+plt.title("Anotación Clasificación")
+plt.imshow(carga_imagen)
+plt.axis("off")
+plt.tight_layout()
+plt.subplot(3,2,4)
+plt.title("Anotación Detección")
+plt.axis("off")
+plt.imshow(carga_imagen)
+plt.tight_layout()
+plt.subplot(3,2,5)
+plt.title("Anotación Segmentación \nSemántica")
+plt.imshow(carga_imagen)
+plt.axis("off")
+plt.subplot(3,2,6)
+plt.title("Anotación Segmentación \nde Instancias")
+plt.axis("off")
+plt.imshow(carga_imagen)
+plt.tight_layout()
 
-
-
-print("Hola intento3")
-
-"""INTENTO 4!!!"""
-
-print("Yo no mando vainas groseras")
-
-#HOLA

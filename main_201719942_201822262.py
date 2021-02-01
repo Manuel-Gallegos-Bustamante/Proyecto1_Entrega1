71cd577bbb77fe61003fe44e804ea03436af8591
Manuel Gallegos Bustamante <m.gallegos@uniandes.edu.co>
2021-01-31 20:42:32 -0500

    v2

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -2,6 +2,7 @@
 #Manuel Gallegos Bustamante #Código: 201719942
 #Análisis y procesamiento de imágenes: Proyecto1 Entrega1
 #Se importan librerías que se utilizarán para el desarrollo del laboratorio
+#Intento 2!!!!
 
 
 

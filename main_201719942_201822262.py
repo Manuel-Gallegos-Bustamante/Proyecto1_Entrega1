ea8972b9e5b7dd82f11d540980774ef0b6425440
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2021-01-31 20:39:16 -0500

    comentarios

---
+++ main_201719942_201822262.py
@@ -0,0 +1,12 @@
+#Pamela Ramírez González #Código: 201822262
+#Manuel Gallegos Bustamante #Código: 201719942
+#Análisis y procesamiento de imágenes: Proyecto1 Entrega1
+#Se importan librerías que se utilizarán para el desarrollo del laboratorio
+
+
+
+
+
+
+
+#HOLA

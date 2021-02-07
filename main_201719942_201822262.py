ddaa6b92410c32c2b3b5c60098ee7ea60e5b5aea
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2021-02-06 20:35:15 -0500

    segunda parte con comentarios

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -28,87 +28,88 @@ plt.imshow(carga_imagen) # imagen original a color
 plt.axis("off")
 plt.subplot(3,2,3)
 plt.title("Anotación Clasificación")
-plt.imshow(io.imread("Clasificacion.png")) # se lee la imagen correspondiente a la anotación de clasificación con io.imread
+plt.imshow(io.imread("Clasificacion.png")) # se carga la imagen correspondiente a la anotación de clasificación con io.imread
 plt.axis("off")
 plt.subplot(3,2,4)
 plt.title("Anotación Detección")
 plt.axis("off")
-plt.imshow(io.imread("Deteccion.jpeg"))# se lee la imagen correspondiente a la anotación de detección con io.imread
+plt.imshow(io.imread("Deteccion.jpeg"))# se carga la imagen correspondiente a la anotación de detección con io.imread
 plt.subplot(3,2,5)
 plt.title("Anotación Segmentación \nSemántica")
-plt.imshow(io.imread("Seg_Semantica.jpeg")) # se lee la imagen correspondiente a la segmentación semántica con io.imread
+plt.imshow(io.imread("Seg_Semantica.jpeg")) # se carga la imagen correspondiente a la segmentación semántica con io.imread
 plt.axis("off")
 plt.subplot(3,2,6)
 plt.title("Anotación Segmentación \nde Instancias")
+plt.imshow(io.imread("Seg_Instancias.jpeg"))# se carga la imagen correspondiente a la segmentación de instancias con io.imread
 plt.axis("off")
-plt.imshow(io.imread("Seg_Instancias.jpeg"))# se lee la imagen correspondiente a la segmentación de instancias con io.imread
 plt.tight_layout() #se utiliza plt.tight_layout() para evitar que se sobrepongan títulos y se ajusten las imágenes
 plt.show() # visualizar la figura con plt.show
-plt.savefig("VisualizaciónAnotaciones",Bbox_inches="tight") # se guarda la figura indicando como 2do parámetro Bbox_inches="tight" para evitar que los bordes blancos sean muy gruesos 
+plt.savefig("VisualizaciónAnotaciones",Bbox_inches="tight") # se guarda la figura indicando como 2do parámetro Bbox_inches="tight" para evitar que los bordes blancos sean muy gruesos
 ##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-monedaURL="https://web.stanford.edu/class/ee368/Handouts/Lectures/Examples/11-Edge-Detection/Hough_Transform_Circles/coins.png"
-monedas = requests.get(monedaURL)
+monedaURL="https://web.stanford.edu/class/ee368/Handouts/Lectures/Examples/11-Edge-Detection/Hough_Transform_Circles/coins.png"  # se asigna a una variable la url de la imagen que se trabajará en la segunda parte del laboratorio
+monedas = requests.get(monedaURL) # se accede a la imagen para su descarga por medio de la url con requests.get
 with open("Monedas", "wb") as f: # se trabaja con f como la abreviación para abrir un archivo para escritura "Monedas"
 	f.write(monedas.content) #se escribe con .write en el archivo previamente mencionado el contenido de la descarga de la imagen realizado previamente con .content
 monedas = io.imread("Monedas") # se carga la imagen del archivo creado con io.imread
-vectorColor = monedas.flatten()
-plt.figure("HistogramaMonedas")
-plt.subplot(1,2,1)
-plt.imshow(monedas,cmap="gray")
-plt.title("Imagen monedas")
-plt.axis('off')
+vectorColor = monedas.flatten()  #se realiza un .flatten() de la imagen en escala de grises para que se trabaje en una dimensión
+plt.figure("HistogramaMonedas") # se crea figura "HistogramaMonedas" con un subplot de 1x2 para almacenar la imagen original y su respectivo histograma
+plt.subplot(1,2,1) # para cada subplot se indican como 1er parámetro el número de filas, como 2do parámetro el número de columnas y como 3er parámetro el índice en el cual irá la imagen o histograma
+plt.imshow(monedas,cmap="gray") # se visualiza la imagen con plt.imshow indicando como segundo parámetro el mapa de color para la visualización el cual en este caso es "gray"
+plt.title("Imagen monedas") # tanto para el histograma como para la imagen se inserta el título con plt.title
+plt.axis('off') # se quitan los ejes
 plt.subplot(1,2,2)
-plt.hist(vectorColor,bins=256)
+plt.hist(vectorColor,bins=256) # para realizar el histograma se trabaja con la imagen en escala de grises vectorizada previamente para que se trabaje en una dimensión. Además se inidica como parámetro bins=256 para que el histograma tenga más divisiones por cada uno de los intermedios entre 0-255 (negro-blanco)
 plt.title('Histograma imagen monedas')
-plt.tight_layout()
-plt.show()
+plt.tight_layout() #se utiliza plt.tight_layout() para evitar que se sobrepongan títulos y se ajusten las imágenes
+plt.show() # visualizar la figura con plt.show
 #plt.savefig("HistogramaMonedas",Bbox_inches="tight")
-plt.savefig("HistogramaMonedas")
-##umbral de binarización de acuerdo al método de Otsu
-#input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-binOtsu=threshold_otsu(monedas)
-monedas_binOtsu=monedas>binOtsu
-#print(binOtsu)
-plt.figure("BinOtsu")
+##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+#umbral de binarización de acuerdo al método de Otsu
+binOtsu=threshold_otsu(monedas) # calculo del umbral por método Otsu con función threshold_otsu
+monedas_binOtsu=monedas>binOtsu # máscasa binaria de la imagen monedas haciendo uso del umbral calculado previamente. pixeles con valores mayores al umbral toman el valor de 1, de lo contrario toman el valor de 0
+#print(binOtsu) # visualización del umbral calculado con el método Otsu
+plt.figure("BinOtsu") # figura de la binarización de la imagen con el método Otsu, se inserta título con plt.title y se visualiza la máscara con plt.imshow con el mapa de color "gray" y se quitan los ejes con plt.axis("off") y se visualiza con plt.show
 plt.title("Binarización de la imagen con Otsu")
 plt.imshow(monedas_binOtsu, cmap="gray")
 plt.axis('off')
-##binarización con percentil 60
-#input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-calculo_percentil60=np.percentile(monedas,60)
-monedas_percentil60=monedas>calculo_percentil60
-#print(calculo_percentil60)
-plt.figure("Percentil 60")
+plt.show()
+##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+#binarización con percentil 60
+calculo_percentil60=np.percentile(monedas,60) # se calcula el percentil 60 haciendo uso de la función percentile de la librería numpy la cual recibe como 1er parámetro la imagen a la cual se le calculará el percentil y como 2do parámetro el número del percentil que se desea calcular
+monedas_percentil60=monedas>calculo_percentil60 # máscasa binaria de la imagen monedas haciendo uso del umbral calculado previamente. pixeles con valores mayores al umbral toman el valor de 1, de lo contrario toman el valor de 0
+#print(calculo_percentil60) # visualización del umbral calculado con el percentil 60
+plt.figure("Percentil 60") # figura de la binarización de la imagen con el percentil 60, se inserta título con plt.title y se visualiza la máscara con plt.imshow con el mapa de color "gray" y se quitan los ejes con plt.axis("off") y se visualiza con plt.show
 plt.title("Binarización de la imagen con percentil 60")
 plt.imshow(monedas_percentil60, cmap="gray")
 plt.axis('off')
+plt.show()
 ##binarización con umbral = 175
 #input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-monedas_umbral175 = monedas > 175
-plt.figure("Umbral 175")
+monedas_umbral175 = monedas > 175 # máscasa binaria de la imagen monedas haciendo uso del umbral 175. pixeles con valores mayores al umbral toman el valor de 1, de lo contrario toman el valor de 0
+plt.figure("Umbral 175") # figura de la binarización de la imagen con el umbral 175,  se inserta título con plt.title y se visualiza la máscara con plt.imshow con el mapa de color "gray" y se quitan los ejes con plt.axis("off") y se visualiza con plt.show
 plt.title("Binarización de la imagen con umbral 175")
 plt.imshow(monedas_umbral175, cmap="gray")
 plt.axis('off')
-##selección de dos umbrales arbitrarios y establecer rango
-#input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-monedas_copia = monedas.copy()
-#monedas_copia = monedas_copia.flatten()
-#print(monedas_copia)
-for i in range(0, len(monedas_copia)):
-	for j in range(0, len(monedas_copia[i])):
-		if monedas_copia[i][j] > 65 and monedas_copia[i][j] < 250:
-			monedas_copia[i][j] = 255
-		else:
-			monedas_copia[i][j] = 0
-plt.figure("Umbral arbitrario")
+plt.show()
+##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+#selección de dos umbrales arbitrarios y establecer rango
+monedas_copia = monedas.copy() # se crea una copia de la imagen modenas
+for i in range(0, len(monedas_copia)): # se realiza un recorrido por las filas de la imagen
+	for j in range(0, len(monedas_copia[i])): # se realiza un recorrido por las columnas de la imagen
+		if monedas_copia[i][j] > 65 and monedas_copia[i][j] < 250: # Se selecciona un umbral de 65-250 para realizar la binarización de la imagen. Por esta razón se crea una condición en la cual si x pixel (indicado como una posición ij de la matriz de la imagen) es mayor al límite inferior del umbral arbitrario escogido y si es menor al límite superior
+			monedas_copia[i][j] = 255 # se cumplirse la condición se asigna al pixel (posición) el color blanco, es decir 255
+		else: # si el pixel no se encuentra dentro del rango de umbrales establecido
+			monedas_copia[i][j] = 0 # se asigna a ese pixel el color negro, es decir 0
+plt.figure("Umbral arbitrario")# figura de la binarización de la imagen con el rango de umbrales arbitrario,  se inserta título con plt.title y se visualiza la máscara con plt.imshow con el mapa de color "gray" y se quitan los ejes con plt.axis("off") y se visualiza con plt.show
 plt.title("Umbral arbitrario") 
 plt.imshow(monedas_copia, cmap='gray')
 plt.axis('off')
-##subplot para máscaras con segmentaciones en escala de grises
-#input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
-plt.figure("MascarasySegmentaciones")
-plt.subplot(2,4,1)
-plt.imshow(monedas_binOtsu,cmap="gray")
+plt.show()
+##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+#subplot para máscaras con segmentaciones en escala de grises
+plt.figure("MascarasySegmentaciones") # se crea figua con subplot de 2x4 para las distintas máscaras binarias generadas anteriormente y sus respectivas segmentaciones
+plt.subplot(2,4,1) # para cada subplot se indican como 1er parámetro el número de filas, como 2do parámetro el número de columnas y como 3er parámetro el índice en el cual irá la máscara o segmentación
+plt.imshow(monedas_binOtsu,cmap="gray") # se visualiza cada una de las máscaras y segmentaciones con imshow y el mapa de color "gray". Además, se le inserta el título con .tittle y se le quitan los ejes con axis("off)
 plt.title("Máscara 1:\nOtsu")
 plt.axis('off')
 plt.subplot(2,4,2)
@@ -124,7 +125,7 @@ plt.imshow(monedas_copia,cmap="gray")
 plt.title("Máscara 4: Umbral\nrango 65-250")
 plt.axis('off')
 plt.subplot(2,4,5)
-plt.imshow(monedas_binOtsu*monedas,cmap="gray")
+plt.imshow(monedas_binOtsu*monedas,cmap="gray") # para realizar las segmentaciones se realiza una multiplicación (elemento por elemento) de la imagen original de monedas y de la máscara correspondiente a la segmentación deseada. Lo anterior debido a que como la máscara es binaria (valores de 0 o 1) aquellos pixeles (elementos) que en la máscara tengan un valor de 1 mantendran su nivel de gris mientras que los que tengan un valor de 0 en la máscara binaria tomarán un valor de 0 es decir negro
 plt.title("Segmentación 1:\nOtsu")
 plt.axis('off')
 plt.subplot(2,4,6)
@@ -139,11 +140,10 @@ plt.subplot(2,4,8)
 plt.imshow(monedas_copia * monedas,cmap="gray")
 plt.title("Segmentación 4: Umbral\nrango 65-250")
 plt.axis('off')
-plt.tight_layout()
-plt.show()
-
-##PROBLEMA BIOMÉDICO
-#input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+plt.tight_layout() #se utiliza plt.tight_layout() para evitar que se sobrepongan títulos y se ajusten las imágenes
+plt.show() # visualizar la figura con plt.show
+##input("Press Enter to continue...") # input para continuar con el programa cuando usuario presione Enter cuando desee
+#PROBLEMA BIOMÉDICO
 archivosresonancias=glob.glob(os.path.join("Heart_Data","Data","*.nii.gz"))
 info = {}
 for i in archivosresonancias:
